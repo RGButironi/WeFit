b90f524e552d412a1f2529a698b9f556ca941fd9
RGButironi <60583063+RGButironi@users.noreply.github.com>
2020-03-12 09:16:06 -0400

    About page version 1.1; specified scope of app

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -7,7 +7,16 @@
       WeFit has done the hard work for you and brought some of the best routines on the web together within a convenient and easy to use app.
       Our staff of virtual trainers scoure the web in search of the best training videos available and constantly
       update WeFit to provide new and exciting routines every week.</p>
-      <p class="column has-text-weight-semibold is-half is-offset-one-quarter">Leverage the power of spontaneity to your benefit; new and exciting routines are at your fingertips with WeFit!</p>
+      <h1 class="has-text-weight-semibold">How WeFit Helps You Reach Your Fitness Goals</h1>
+      <div class="column is-one-third is-offset-one-third">
+      <ol type="1">
+        <li>Our administrators refresh our video workout routines offered in the "Workout" section each day.</li>
+        <li>You can choose a routine based not only on its category but also its diffculty.</li>
+        <li>You can add your favorite routines to your profile page and share with friends in the app.</li>
+        <li>Once you complete a routine you progress will appear in your profile for others to see.</li>
+      </ol>
+      </div>
+      <p class="column has-text-weight-semibold is-half is-offset-one-quarter">Leverage the power of spontaneity and social-media to your benefit; new and exciting routines are at your fingertips with WeFit!</p>
   </div>
   </div>
 </template>
